5bb1028f0137c680303d16880a60624ecd1b988d
Kárpáti Zoltán <karpatizoltan96@gmail.com>
2022-05-16 14:37:27 +0200

    Remove unnecessary code.

--- Questionnaire.Api/ClientApp/src/app/app-routing.module.ts
+++ Questionnaire.Api/ClientApp/src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ForbiddenComponent } from './forbidden/forbidden.component';
@@ -12,8 +10,6 @@ const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full' },
     { path: 'not-found', component: NotFoundComponent, pathMatch: 'full' },
     { path: 'forbidden', component: ForbiddenComponent },
-    { path: 'counter', component: CounterComponent },
-    { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
     { path: 'group',
       loadChildren: () => import('./features/group/group.module')
         .then(m => m.GroupModule), canActivate: [AuthorizeGuard] },
